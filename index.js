04b8a91cae59266702873757b7874e5d160881cf
Gilbert J. Camarillo III <gil.camarillo73@gmail.com>
2021-09-12 03:09:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+ 
+    // Your code here.
+    console.log("Fart")
+ 
+});
+
+$( "#myButt" ).click(function() {
+ 
+    // Your code here.
+    alert( "welcome" );
+ 
+});
